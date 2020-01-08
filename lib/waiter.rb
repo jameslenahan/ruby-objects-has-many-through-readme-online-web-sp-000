ceff95c6044f5982cc13f0f64b76d1f40a403e75
James Lenahan <james.k.lenahan@gmail.com>
2020-01-08 19:38:01 +0000

    Automatically backed up by Learn

--- lib/waiter.rb
+++ lib/waiter.rb
@@ -1,5 +1,17 @@
-def waiters
-  meals.map do |meal|
-    meal.waiter
+class Waiter
+ 
+  attr_accessor :name, :yrs_experience
+ 
+  @@all = []
+ 
+  def initialize(name, yrs_experience)
+    @name = name
+    @yrs_experience = yrs_experience
+    @@all << self
   end
+ 
+  def self.all
+    @@all
+  end
+ 
 end
